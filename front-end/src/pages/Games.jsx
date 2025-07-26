97d9d5b8177e856c0a20d898522147af1f728cba
Tamra Chan <tamrachan@gmail.com>
2025-07-26 20:12:05 +0100

    Added game cards to Games menu

--- front-end/src/pages/Games.jsx
+++ front-end/src/pages/Games.jsx
@@ -1,11 +1,21 @@
-// import "../css/Games.css"
+import "../css/Games.css"
+import redBeachBall from '../icons/red_beach_ball.png';
 
 function Games() {
 
     return (
         <div className="game-menu">
             <h1>Mini games</h1>
+            
             <p>Add mini games here</p>
+            <div className="game-grid">
+                <div className="game-card">
+                    <h3>TicTacToe</h3>
+                    <h4>2 players</h4>
+                    <img className="game-img" src={redBeachBall}></img>
+                    <p className="game-desc">Battle it out over a two-player TicTacToe game</p>
+                </div>
+            </div>
         </div>
     )
 }
